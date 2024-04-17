2077b39a5045ba8c52410877f70b7ba42621b937
Chris Mainey <c.mainey1@nhs.net>
2024-04-17 17:25:02 +0100

    first commit

---
+++ cvd_prevent.R
@@ -0,0 +1,14 @@
+################################################################################
+# CVD Prevent data extraction
+# Using this to test out connection to Powerpoint through OfficeR package
+
+# Chris Mainey - c.mainey1@nhs.net
+# 17/04/2024
+################################################################################
+
+# Connect to SQL Server.
+
+
+
+# Powerpoint import
+
